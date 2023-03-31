79a0a9fcddc091bb2ad7e8eecc556355b72fd379
Vincent Moucadeau <v.moucadeau@gmail.com>
2023-03-31 17:52:21 +0200

    add divpol and modpol starting euclidepol (not working)

--- GF.hs
+++ GF.hs
@@ -44,11 +44,11 @@ instance Field a => Field (GF2 a) where
 -- Exemple : [1 0 1 0 1 0 0 1] = 1 + x² + x⁴ + x⁷
 newtype Polynome a = Pol [a] deriving (Show)
 
-addPol :: Num a => Polynome a -> Polynome a -> Polynome a
-addPol (Pol a) (Pol b) = Pol (opList (+) 0 a b)
+addpol :: Num a => Polynome a -> Polynome a -> Polynome a
+addpol (Pol a) (Pol b) = Pol (opList (+) 0 a b)
 
-subPol :: Num a => Polynome a -> Polynome a -> Polynome a
-subPol (Pol a) (Pol b) = Pol (opList (-) 0 a b)
+subpol :: Num a => Polynome a -> Polynome a -> Polynome a
+subpol (Pol a) (Pol b) = Pol (opList (-) 0 a b)
 
 addzero :: Num a => Polynome a -> Polynome a
 addzero (Pol a) = Pol (0:a)
@@ -56,45 +56,47 @@ addzero (Pol a) = Pol (0:a)
 oppPol :: Num a => Polynome a -> Polynome a
 oppPol (Pol a) = Pol $ map (\x -> -x) a
 
-multPol :: Num a => Polynome a -> Polynome a -> Polynome a
-multPol (Pol []) _ = Pol [0]
-multPol _ (Pol []) = Pol [0]
-multPol (Pol (x:xs)) (Pol b) = addPol (Pol (map (x *) b)) (addzero (multPol (Pol xs) (Pol b)))
+multpol :: Num a => Polynome a -> Polynome a -> Polynome a
+multpol (Pol []) _ = Pol [0]
+multpol _ (Pol []) = Pol [0]
+multpol (Pol (x:xs)) (Pol b) = addpol (Pol (map (x *) b)) (addzero (multpol (Pol xs) (Pol b)))
 
--- degPol :: Num a => Polynome a -> Int
--- degPol (Pol [0]) = -1
--- degPol (Pol []) = -1
--- degPol (Pol list) = (length list) - 1
+degpol :: Eq a => Num a => Polynome a -> Int
+degpol (Pol []) = -1
+degpol (Pol (x:xs)) | x /= 0 = 1 + degpol (Pol xs)
+                    | otherwise = 0 + degpol (Pol xs)
 
 show_pol :: Polynome Float -> String
 show_pol (Pol (x1:x2:xs)) = "P(x) = " ++ show x1 ++ " + " ++ show x2 ++ "x + " ++ intercalate " + " (zipWith (\c n -> show c ++ "x^" ++ show n) xs [2..length xs - 1])
 
-
-tailPol :: Num a => Polynome a -> Polynome a
-tailPol (Pol (x:xs)) = Pol (xs)
-
 toList :: Num a => Polynome a -> [a]
 toList (Pol list) = list
 
--- divPol :: Fractional a => Polynome a -> Polynome a -> Polynome a
--- divPol (Pol a) (Pol b) | degPol (Pol a) >= degPol (Pol b) = Pol (divPol' a b)
---                        | otherwise = Pol([0])
---     where divPol' (x:xs) y 
+divpol :: Fractional a => Polynome a -> Polynome a -> Polynome a
+divpol (Pol a) (Pol b) = Pol (reverse $ divpol' (reverse a) (reverse b))
+    where divpol' (x:xs) y | length (x:xs) < length y = []
+                           | otherwise = x/head y : (divpol' (tail(toList(subpol (Pol (x:xs)) (multpol (Pol [x/head y]) (Pol y))))) y)
+          divpol' [] _ = []
 
--- modPol :: Fractional a => Polynome a -> Polynome a -> Polynome a
--- modPol (Pol a) (Pol b) = Pol (reverse $ modPol' (reverse a) (reverse b))
---     where modPol' (x:xs) y | length (x:xs) < length y = []
---                            | otherwise = (x/head y) : toList (modPol ( (tailPol (subPol (Pol (x:xs)) (multPol (Pol [x/head y]) Pol(y))))  ))
---           modPol' [] _ = []
+modpol :: Fractional a => Polynome a -> Polynome a -> Polynome a
+modpol (Pol a) (Pol b) = Pol (reverse $ modpol' (reverse a) (reverse b))
+    where modpol' (x:xs) y | length (x:xs) < length y = (x:xs)
+                           | otherwise = (modpol' (tail(toList(subpol (Pol (x:xs)) (multpol (Pol [x/head y]) (Pol y))))) y)
+          modpol' [] _ = []
+
+-- euclidepol :: Num a => Polynome a -> Polynome a -> (Polynome a, Polynome a, Polynome a)
+-- euclidepol (Pol a) (Pol b) | b == [1] = ((Pol a), Pol [1], Pol [0])
+--                            | otherwise = (d', v', subpol(u' (multpol v' (divpol((Pol a) (Pol b)))) ) )
+--                 where (d', v', u') = euclidepol ( (Pol b) (modpol((Pol a) (Pol b))) ) 
 
 instance Group (Polynome Integer) where
     zer = Pol [0]
     opp = oppPol
-    add = addPol
+    add = addpol
 
 instance Ring (Polynome Integer) where
     one = Pol [1]
-    mul = multPol
+    mul = multpol
 
 -- instance Show Polynome where
 --     show = show_pol
