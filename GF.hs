11162341ddce4a31e2fb86b752d271c0041dca2e
DrIco12 <AlexVicoINP@gmail.com>
2023-04-02 11:56:04 +0200

    Finishing field GF256
    I cracked so now it'w a field

--- GF.hs
+++ GF.hs
@@ -4,12 +4,11 @@ module GF where
 import Struct
 import Scalaire
 
-----------------------------------------------------------
--- ---------- Polynômes d'exemple pour GF256 ---------- --
-----------------------------------------------------------
+newtype GF256 = F8 [Zs2Z]
 
-empty :: a -> a
-empty x = x
+----------------------------------------------------------
+-- ---------- Polynômes d'exemple dans GF256 ---------- --
+----------------------------------------------------------
 
 ex0 :: GF256
 ex0 = F8 []
@@ -24,7 +23,7 @@ ex3 :: GF256
 ex3 = F8 [one,one,one,zer,one,zer,one,zer]
 
 ex4 :: GF256
-ex4 = F8 [one,one,zer,zer,one,zer,zer,zer]
+ex4 = F8 [one,one,zer,zer,one,zer,zer]
 
 ex5 :: GF256
 ex5 = F8 [zer,one,one,one,one,one,one,one]
@@ -33,25 +32,43 @@ mx :: GF256
 mx = F8 [one,one,zer,one,one,zer,zer,zer,one]
 
 ----------------------------------------------------------
--- ---------- Fonctions secondaires de GF256 ---------- --
+-- --------- Fonctions secondaires pour GF256 --------- --
 ----------------------------------------------------------
 
-clearf8 :: GF256 -> GF256
-clearf8 (F8 (x0:x1:x2:x3:x4:x5:x6:x7:xs)) = F8 (x0:x1:x2:x3:x4:x5:x6:x7:[])
-clearf8 x = x
+apply :: Int -> (a -> a) -> a -> a
+apply 0 _ x = x
+apply n f x = apply (n-1) f (f x)
+
+wipef8 :: GF256 -> GF256
+wipef8 (F8 []) = F8 []
+wipef8 (F8 x) | last x == zer = wipef8 (F8 (init x)) | otherwise = F8 x
 
 multbyx :: GF256 -> GF256
 multbyx (F8 (x0:x1:x2:x3:x4:x5:x6:(Z2Z 0):[])) = F8 (zer:x0:x1:x2:x3:x4:x5:x6:[])
-multbyx (F8 (x0:x1:x2:x3:x4:x5:x6:(Z2Z 1):[])) = clearf8 $ subs (F8 (zer:x0:x1:x2:x3:x4:x5:x6:[one])) mx
+multbyx (F8 (x0:x1:x2:x3:x4:x5:x6:(Z2Z 1):[])) = wipef8 $ subs (F8 (zer:x0:x1:x2:x3:x4:x5:x6:[one])) mx
 multbyx (F8 x) | length x < 8 = F8 (zer:x)
+
+degf8 :: GF256 -> Int
+degf8 x = let (F8 y) = wipef8 x in length y - 1
+
+divEf8 :: GF256 -> GF256 -> (GF256,GF256)
+divEf8 x y | degf8 x < degf8 y = (zer,wipef8 x)
+divEf8 x y = let f = apply (degf8 x - degf8 y) (\(F8 x)->F8(zer:x)) in let (q,r) = (divEf8 (subs x (f y)) y) in (add (f one) q,r)
+
+truemul :: GF256 -> GF256 -> GF256
+truemul (F8 []) _ = zer
+truemul (F8 (x:xs)) (F8 y) = add (F8 (map (mul x) y)) ((\(F8 x)->F8(zer:x)) $ truemul (F8 xs) (F8 y))
+
+euclidf8 :: (GF256,GF256,GF256) -> (GF256,GF256,GF256) -> (GF256,GF256,GF256)
+euclidf8 x (_,_,y) | y == zer = x
+euclidf8 (x0,x1,or) (y0,y1,nr) = let (z,_) = divEf8 or nr in euclidf8 (y0,y1,nr) (subs x0 (mul z y0),subs x1 (mul z y1),wipef8 $ subs or (truemul z nr))
 
 ----------------------------------------------------------
 -- ---------- Définition du corps fini GF256 ---------- --
 ----------------------------------------------------------
 
-newtype GF256 = F8 [Zs2Z]
-
 f8show :: GF256 -> String
+f8show (F8 []) = " But it's empty... "
 f8show (F8 x) = foldl (++) [] (map show (reverse x))
 
 f8eq :: GF256 -> GF256 -> Bool
@@ -71,10 +88,10 @@ f8one = F8 [one]
 
 f8mul :: GF256 -> GF256 -> GF256
 f8mul (F8 []) _ = zer
-f8mul (F8 (x:xs)) (F8 y) = f8add (F8 (map (mul x) y)) (multbyx $ f8mul (F8 xs) (F8 y))
+f8mul (F8 (x:xs)) (F8 y) = wipef8 $ add (F8 (map (mul x) y)) (multbyx $ f8mul (F8 xs) (F8 y))
 
 f8inv :: GF256 -> GF256
-f8inv (F8 x) = zer -- I'll do it next week, don't you dare steal it ^^
+f8inv x | (&&) (degf8 x - 1 < 7) (degf8 x + 1 > 0) = let (_,z,_) = euclidf8 (one,zer,mx) (zer,one,x) in wipef8 z
 
 instance Show GF256 where
     show = f8show
